0d858eaf68e7635bba75ce700cd77370e2cf9a19
Aayush Gupta <aayushgupta2097@gmail.com>
2019-05-23 11:49:27 +0530

    adding reset email template

--- Xandar/Xandar/urls.py
+++ Xandar/Xandar/urls.py
@@ -60,7 +60,9 @@ urlpatterns = [
 
     path('password-reset', 
     	auth_views.PasswordResetView.as_view(
-    		template_name='accounts/password_reset.html'),
+    		template_name='accounts/password_reset.html', html_email_template_name="accounts/email_reset_template.html",
+            email_template_name='accounts/password_reset_email.html', subject_template_name="accounts/password_reset_subject.txt",
+             ),
     		 name="password_reset"),
 
     path('password-reset/done', 
